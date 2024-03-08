ca407de61d56d08ea1d1846fd730f884876cb5ca
yaggod <yaggod@yandex.ru>
2024-03-08 07:04:57 +0300

    moved \n symbols to the end of echo lines

--- Task1/code/index.php
+++ Task1/code/index.php
@@ -9,29 +9,29 @@ $order = &$very_bad_unclear_name;
 $order .= " and water";
 
 // Don't change the line below
-echo "\nYour order is: $very_bad_unclear_name.";
+echo "Your order is: $very_bad_unclear_name.\n";
 
 
 $int = 123;
-echo "\n$int";
+echo "$int\n";
 
 $float = 12.3;
-echo "\n$float";
+echo "$float\n";
 
 $value = 11 + 1;
-echo "\n$value";
+echo "$value\n";
 
 $lastMonth = 1187.23;
 $thisMonth = 1089.98;
-echo "\nLast month i've spent ", $lastMonth - $thisMonth, " more";
+echo "Last month i've spent ", $lastMonth - $thisMonth, " more\n";
 
 $numLanguages = 3; // i think that languagesCount is way better name for this but ok
 $months = 11; // monthsSpent ?
 $days = $months * 16; // ^
 $daysPerLanguage = $days / $numLanguages;
-echo "\n\nMegg spent $daysPerLanguage on average for each language";
+echo "Megg spent $daysPerLanguage on average for each language\n";
 
-echo "\n", 8**2;
+echo 8**2, "\n";
 
 $my_num = 0;
 $answer = $my_num;
@@ -40,19 +40,19 @@ $answer *= 2;
 $answer -= 2;
 $answer /= 2;
 $answer -= $my_num;
-echo "\n$answer";
+echo "$answer\n";
 
 $a = 10;
 $b = 3;
-echo "\na % 3 is ", $a % $b;
+echo "a % 3 is ", $a % $b, "\n";
 
 if($a % $b == 0)
 {
-    echo "\n$a is dividable by $b and {$a}/{$b}=", $a / $b;
+    echo "$a is dividable by $b and {$a}/{$b}=", $a / $b, "\n";
 }
 else
 {
-    echo "\n$a is not dividable by $b and {$a}%{$b}=", $a % $b;
+    echo "$a is not dividable by $b and {$a}%{$b}=", $a % $b, "\n";
 }
 
 $st = pow(2, 10);
@@ -61,5 +61,5 @@ $squareRoot = sqrt(245);
 $values = [4, 2, 5, 19, 13, 0, 10];
 foreach($values as $arrayItem)
 {
-    echo "\nSquare root of $arrayItem is ", sqrt($arrayItem);
+    echo "Square root of $arrayItem is ", sqrt($arrayItem), "\n";
 }
