7bec3f167d27a7c8524056cdfd0f88544b57e4b6
yaggod <yaggod@yandex.ru>
2024-03-26 02:27:24 +0300

    changed comment

--- Task3/code/regex.php
+++ Task3/code/regex.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$firstPattern = "/a..b/"; // the task says that the correct pattern is a..b, but i think that it is just a typo
+$firstPattern = "/a..b/"; // the task says that the correct pattern is a..b with a..a examples, but i think that it is just a typo
 $str = 'ahb acb aeb aeeb adcb axeb';
 
 $count = preg_match_all($firstPattern, $str, $matches);
