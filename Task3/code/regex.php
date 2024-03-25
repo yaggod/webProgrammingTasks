b3c9a110519fa8656624fdc9ea9f72e46ace0f62
yaggod <yaggod@yandex.ru>
2024-03-26 02:26:19 +0300

    finished regex subtask 1

--- Task3/code/regex.php
+++ Task3/code/regex.php
@@ -1,5 +1,8 @@
 <?php
 
 
-$firstPattern = "a..a"; // the task says that the correct pattern is a..b, but i think that it is just a typo
+$firstPattern = "/a..b/"; // the task says that the correct pattern is a..b, but i think that it is just a typo
 $str = 'ahb acb aeb aeeb adcb axeb';
+
+$count = preg_match_all($firstPattern, $str, $matches);
+print_r($matches);
