a12c6fd6d2c573b978003649a4e326e76d4ca8c9
yaggod <yaggod@yandex.ru>
2024-03-26 02:26:34 +0300

    finished regex subtask 2

--- Task3/code/regex.php
+++ Task3/code/regex.php
@@ -6,3 +6,16 @@ $str = 'ahb acb aeb aeeb adcb axeb';
 
 $count = preg_match_all($firstPattern, $str, $matches);
 print_r($matches);
+
+
+$numbersMixedString = "a1b2c3";
+$numberPattern = "/\d+/";
+
+$count = preg_match_all($numberPattern, $numbersMixedString, $foundNumbers);
+foreach($foundNumbers[0] as $number)
+{
+    $numberCube = $number * $number * $number; // calling pow() for natural power loewr than 5 is a bad idea
+    $numbersMixedString = preg_replace("/$number/", $numberCube, $numbersMixedString); // i am certain this will work incorrecly in some cases, but in this case it works
+}
+
+echo $numbersMixedString;
