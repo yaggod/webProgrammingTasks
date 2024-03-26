bcaeb9c902873cfedbad26cd5c866a2fcb43ef94
yaggod <yaggod@yandex.ru>
2024-03-26 23:24:36 +0300

    minor changes in submission handling

--- Task3/code/createListing.php
+++ Task3/code/createListing.php
@@ -2,21 +2,28 @@
 if(!isset($_POST["email"], $_POST["category"], $_POST["header"], $_POST["description"], ))
     redirectToMainPage();
 
+$email = $_POST["email"];
 $category = $_POST["category"];
 $header = $_POST["header"];
 $description = $_POST["description"];
 
 
-$filepath = "categories/{$category}/{$header}.txt";
 
-if(!file_put_contents($filepath, $description))
-    throw new Exception("Unable to access file {$filepath}");
+$directoryPath = "categories/{$category}/{$email}/";
+$filename = "{$header}.txt";
+if(!is_dir($directoryPath))
+    mkdir($directoryPath);
 
+$filePath = "{$directoryPath}/{$filename}";
+if(!file_put_contents($filePath, $description))
+{
+    throw new Exception("Unable to acess file ".  $filePath);
+}
 
 redirectToMainPage();
 
 function redirectToMainPage()
 {
-    header("location: market.php");
+    //header("location: market.php");
     exit();
 }
