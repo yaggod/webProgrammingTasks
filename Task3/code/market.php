5a9dbf4f9885207d4ceff413dfcc1f81c09cd35c
yaggod <yaggod@yandex.ru>
2024-03-26 22:57:12 +0300

    created form without any logics

--- Task3/code/market.php
+++ Task3/code/market.php
@@ -5,7 +5,18 @@
 </head>
 <body>
     <form method="post" action="createListing.php">
-
+        <label for="email">Your email:</label>
+        <input type="email" id="email" name="email"><br>
+        <label for="category">Category:</label>
+        <select id="category" name="category">
+            <option value="cars">Cars</option>
+            <option value="clothes">Clothes</option>
+            <option value="electronics">Electronics</option>
+        </select><br>
+        <label for="header">Header:</label>
+        <input id="header" name="header"><br>
+        <label for="description">Product description</label>
+        <textarea id="description" name="description"></textarea>
     </form>
 </body>
 </html>
