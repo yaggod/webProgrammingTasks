23183fe2b6c8a4a5f0edf5167cfce616d654489e
yaggod <yaggod@yandex.ru>
2024-03-26 14:03:17 +0300

    edited index.php content

--- Task3/code/index.php
+++ Task3/code/index.php
@@ -1,2 +1,23 @@
-<?php
-echo phpinfo();
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Main Page</title>
+    </head>
+    <body>
+        <div>
+
+            <form method="post" action="handleCookies.php">
+                <label for="text">Sessions and Cookies</label><br>
+                <textarea id="text" name="text"></textarea><br>
+                <input type="submit" />
+            </form>
+            <?php
+                if(!isset($_COOKIE["message"]))
+                    return;
+                $message = $_COOKIE["message"];
+                echo str_word_count($message), "<br>";
+                echo strlen($message), "<br>";
+            ?>
+        </div>
+    </body>
+</html>
