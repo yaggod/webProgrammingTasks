c3c0494a0e802fba5853d32b707a920cc3bbd123
yaggod <yaggod@yandex.ru>
2024-03-26 14:21:13 +0300

    some additions to the main page

--- Task3/code/index.php
+++ Task3/code/index.php
@@ -4,8 +4,9 @@
         <title>Main Page</title>
     </head>
     <body>
+    <h1>Session and Cookies</h1>
         <div>
-            <h1>Session and Cookies</h1>
+            <h2>Cookies</h2>
             <form method="post" action="handleCookies.php">
                 <label for="text">Enter your text</label><br>
                 <textarea id="text" name="text"></textarea><br>
@@ -18,6 +19,24 @@
                 echo str_word_count($message), "<br>";
                 echo strlen($message), "<br>";
             ?>
+        </div>
+        <div>
+            <h2>Sessions</h2>
+            <h3>Sessions. Part 1</h3>
+            <div>
+                <label>Tell us about you</label>
+                <form method="post" action="handlePerson.php">
+                    <label for="surname">Surname</label>
+                    <input id="surname" name="surname"/><br>
+                    <label for="name">Name</label>
+                    <input id="name" name="name"/><br>
+                    <label for="age">Age</label>
+                    <input id="age" type="number" name="age"/><br>
+
+                    <input type="submit"/>
+                </form>
+            </div>
+
         </div>
     </body>
 </html>
