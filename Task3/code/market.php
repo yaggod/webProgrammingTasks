7d203be94ad8d6a56894ac5731b2dcfbe04db060
yaggod <yaggod@yandex.ru>
2024-03-27 00:06:48 +0300

    fill the table with the listings

--- Task3/code/market.php
+++ Task3/code/market.php
@@ -26,13 +26,60 @@
     <h1>Existing listings</h1>
     <table border="1px">
         <thead>
-            <th>Seller's name</th>
+            <th>Product name</th>
             <th>Category</th>
             <th>Description</th>
             <th>Seller's email</th>
         </thead>
-    </table>
+        <tbody>
+        <?php
+        $categories = array_slice(scandir("categories"), 2);
+        /*
+         pretty ugly loop. can get better by extracting function from the body, but that will cause creation of similar functions like
+        doSmth(a)
+        doSmth(a, b) etc
 
+        can be implemented recursively but that will break all the abstractions
+        */
+        foreach($categories as $category)
+        {
+            $emailsFolder = "categories/{$category}";
+            if(!is_dir($emailsFolder))
+                continue;
+
+            $emails = array_slice(scandir($emailsFolder), 2);
+            foreach($emails as $email)
+            {
+                $listingsFolder = "{$emailsFolder}/{$email}";
+                if(!is_dir($listingsFolder))
+                    continue;
+
+                $listings = array_slice(scandir($listingsFolder), 2);
+                foreach($listings as $listing)
+                {
+                    printListing($category, $email, $listing, $listingsFolder);
+                }
+            }
+        }
+        function printListing($category, $email, $listing, $folder)
+        {
+            $description = file_get_contents("{$folder}/{$listing}");
+
+            $name = substr($listing, 0, -4);
+            printRow($name, $category, $description, $email);
+        }
+        function printRow($name, $category, $description, $email)
+        {
+            echo "<tr>";
+            echo "<td>{$name}</td>";
+            echo "<td>{$category}</td>";
+            echo "<td>{$description}</td>";
+            echo "<td>{$email}</td>";
+            echo "</tr>";
+        }
+        ?>
+        </tbody>
+    </table>
 </div>
 </body>
 </html>
